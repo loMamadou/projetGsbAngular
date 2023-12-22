4adad774dbd094c5ef3f0c70498d5e73fc905f02
mlo00 <mamadou.lo@lmdsio.fr>
2023-12-22 15:40:28 +0100

    lister OK

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import {ListeFraisComponent} from "./frais/liste-frais/liste-frais.component";
 import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
-
+    { path: '', component: HomeComponent },
     {path: 'frais/liste', component: ListeFraisComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: 'home', component: HomeComponent }
 
 ];
 
