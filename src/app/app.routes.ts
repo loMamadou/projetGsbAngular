33ee031172bc051fca461fb8554a5813acd7d419
mlo00 <mamadou.lo@lmdsio.fr>
2023-12-22 11:55:25 +0100

    lister en cours

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ListeFraisComponent} from "./frais/liste-frais/liste-frais.component";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'frais/liste', component: ListeFraisComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+
+];
+
+
+
